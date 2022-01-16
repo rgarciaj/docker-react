79400986f384452b8d936d486c8e5a357ef89895
ricardo-vl <97043371+ricardo-vl@users.noreply.github.com>
2022-01-15 22:46:29 -0300

    text change

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Text change.
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!
         </a>
       </header>
     </div>
