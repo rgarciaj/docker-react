02d1eba8e13b933de2baffb8bb2c88265d91d404
ricardo-vl <97043371+ricardo-vl@users.noreply.github.com>
2022-01-16 22:00:22 -0300

    change text again

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello World!</p>
+        <p>Hello World Again!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
